fb4f62110b17dfda5a6cafab84f33d9c69e345a2
Koushik-Zzz <Koushik02269@gmail.com>
2025-08-02 14:09:05 +0530

    add: Home page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,17 @@
-import Navbar from "@/components/layout/Navbar";
+import Home from "@/components/Home"
+import Navbar from "@/components/layout/Navbar"
 
 export default function HomePage() {
   return (
     <div className="">
       <Navbar />
+      <Home
+        text="hyperbyte"
+        description={[
+          "We create products that serves both users",
+          "and businesses and drive real results.",
+        ]}
+      />
     </div>
-  );
+  )
 }
